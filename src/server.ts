8147311307d5408310aa9af9c1de0522973e6b60
eliton <elitonr65@gmail.com>
2024-06-01 19:46:55 -0300

    Filtrar filmes por gênero

--- src/server.ts
+++ src/server.ts
@@ -104,6 +104,30 @@ app.delete('/movies/:id', async (req, res) => {
     res.status(200).send();
 });
 
+app.get("/movies/:genreName", async(req, res) => {
+    try {
+           const moviesFilteredByGenreName = await prisma.movies.findMany({
+               include: {
+                    genres: true,
+                    languages: true,
+                },
+                where: {
+                    genres: {
+                            name: {
+                                 equals: req.params.genreName,
+                                  mode: "insensitive",
+                             },
+                    },
+                 },
+            });
+
+                   res.status(200).send(moviesFilteredByGenreName);
+   } catch (error) {
+       return res.status(500).send({ message: "Falha ao atualizar um filme" });
+   }
+
+});
+
 app.listen(port, () => {
     console.log(`Servidor em execução em http://localhost:${port}`);
 })
